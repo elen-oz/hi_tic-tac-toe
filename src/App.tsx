98bb9381577561ff9df635f49b99e3d498267cd2
elen-oz <ozerova.lenka@gmail.com>
2024-01-02 19:11:17 +0100

    feat: change layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Game from './components/Game';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,8 +6,10 @@ import Header from './components/Header';
 function App() {
   return (
     <Flex direction='column' justify='space-between' minHeight='100vh'>
-      <Header />
-      <Game />
+      <Box>
+        <Header />
+        <Game />
+      </Box>
       <Footer />
     </Flex>
   );

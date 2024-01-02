450beb466cfb5a603d620a844734dbe75582cd81
elen-oz <ozerova.lenka@gmail.com>
2024-01-02 18:59:31 +0100

    refactor: cleanup

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,39 +5,32 @@ import Board from './Board';
 type Square = string | null;
 
 const Game = () => {
-  // const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState<Square[][] | null>([
     Array(9).fill(null),
   ]);
   const [currentMove, setCurrentMove] = useState(0);
-  // const currentSquares = history[currentMove];
-  const xIsNext = currentMove % 2 === 0;
-  let currentSquares;
 
+  const xIsNext = currentMove % 2 === 0;
+
+  let currentSquares;
   if (history) {
     currentSquares = history[currentMove];
   }
 
   const handlePlay = (nextSquares: Square[]) => {
-    // setHistory((prevHistory) =>
-    //   prevHistory ? [...prevHistory, nextSquares] : [nextSquares]
-    // );
     let nextHistory: Square[][] | null;
     if (history) {
       nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
       setHistory(nextHistory);
       setCurrentMove(nextHistory.length - 1);
     }
-
-    // setXIsNext(!xIsNext);
   };
 
   const jumpTo = (nextMove: number) => {
     setCurrentMove(nextMove);
-    // setXIsNext(nextMove % 2 === 0);
   };
 
-  const moves = history?.map((squares, move) => {
+  const moves = history?.map((_squares, move) => {
     let description: string;
     if (move > 0) {
       description = 'Go to move #' + move;
@@ -45,10 +38,7 @@ const Game = () => {
       description = 'Go to game start';
     }
     return (
-      // <li key={move}>
-      //   <Button onClick={() => jumpTo(move)}>{description}</Button>
-      // </li>
-      <ListItem key={move}>
+      <ListItem key={move} my={1}>
         <Button onClick={() => jumpTo(move)}>{description}</Button>
       </ListItem>
     );
